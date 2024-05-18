1cb7c3117fb81472c0bca923c500c1ce0532d976
Longho Bernard Che <bernard.c.longho@dektech.com>
2024-05-18 14:43:03 +0200

    loops and iterations examples

---
+++ loops-and-iterations/break-statement-with-for-loop.py
@@ -0,0 +1,4 @@
+for i in range(10):
+    if i == 5: # Stop when the value of i is equal to 5
+        break
+    print(i)
---
+++ loops-and-iterations/break-statement-with-while-loop.py
@@ -0,0 +1,6 @@
+count = 0
+while count < 10: 
+    print(count) # print count
+    count += 1 # Add 1 to the value of count
+    if count == 5: # Stop when the value of count is equal to 5
+        break
